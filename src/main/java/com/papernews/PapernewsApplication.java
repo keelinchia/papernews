a63f42c34cd7ac75366f0a8159a9c3b401172e2b
kee <keelin@iastate.edu>
2020-01-17 14:56:13 -0600

    initial commit

---
+++ src/main/java/com/papernews/PapernewsApplication.java
@@ -0,0 +1,13 @@
+package com.papernews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapernewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapernewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/papernews/PapernewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papernews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapernewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
